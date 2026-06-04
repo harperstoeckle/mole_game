d65740a1a494c0ebad34db2c8c3d42967be0d145
Harper Stoeckle <harper.stoeckle@gmail.com>
2026-06-04 16:47:25 -0600

    Allow one-shot particle effects to be emitting in the editor

--- scripts/on_ready_gpu_particles_2d.gd
+++ scripts/on_ready_gpu_particles_2d.gd
@@ -1,7 +1,8 @@
 class_name OnReadyGPUParticles2D
 extends GPUParticles2D
-## Starts emitting when ready.
+## One-shot particle effect that plays when ready.
 
 
 func _ready() -> void:
+	one_shot = true
 	emitting = true
