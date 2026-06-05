42c9df03ceb35c96c8ffa6b9e995cae491bc06cd
Harper Stoeckle <harper.stoeckle@gmail.com>
2026-06-04 19:19:24 -0600

    Enter the ground when colliding while moving fast enough

--- scripts/player.gd
+++ scripts/player.gd
@@ -20,10 +20,16 @@ const IN_GROUND_COLLISION_MASK: int = 0b10
 @export var air_strafe_accel: float = 1000
 @export var floor_friction: float = 0.9
 
+@export_group("dig")
+## We must be moving at least this fast to enter the ground on contact.
+@export var min_speed_to_enter_ground: float = 1000
+@export var dig_speed: float = 1000
+
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var jump_hold_timer: Timer = $JumpHoldTimer
 @onready var leave_ground_detection_area: Area2D = $LeaveGroundDetectionArea
+@onready var dig_effect_spawner: EffectSpawner = $DigEffectSpawner
 
 
 var _has_jumped_in_air := false
@@ -33,7 +39,7 @@ var _is_in_ground := false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
-		if is_on_floor() or not _has_jumped_in_air:
+		if not _is_in_ground and (is_on_floor() or not _has_jumped_in_air):
 			animation_player.stop()
 			animation_player.play("jump")
 			velocity.y = -jump_speed
@@ -52,27 +58,37 @@ func _physics_process(delta: float) -> void:
 	if _is_in_ground and not leave_ground_detection_area.get_overlapping_bodies():
 		leave_ground()
 
-	if not is_on_floor():
-		var accel_factor: float = 1.0
-		if fall_accel_curve:
-			accel_factor = fall_accel_curve.sample_baked(velocity.y)
-		if _is_holding_jump and velocity.y < 0:
-			accel_factor *= jump_hold_gravity_factor
-		velocity.y += accel_factor * fall_accel * delta
-	else:
-		_has_jumped_in_air = false
+	if not _is_in_ground:
+		if not is_on_floor():
+			var accel_factor: float = 1.0
+			if fall_accel_curve:
+				accel_factor = fall_accel_curve.sample_baked(velocity.y)
+			if _is_holding_jump and velocity.y < 0:
+				accel_factor *= jump_hold_gravity_factor
+			velocity.y += accel_factor * fall_accel * delta
+		else:
+			_has_jumped_in_air = false
 
-	var target_x_speed: float = Input.get_axis("left", "right") * (floor_walk_speed if is_on_floor() else air_strafe_speed)
-	var x_accel: float = floor_walk_accel if is_on_floor() else air_strafe_accel
+		var target_x_speed: float = Input.get_axis("left", "right") * (floor_walk_speed if is_on_floor() else air_strafe_speed)
+		var x_accel: float = floor_walk_accel if is_on_floor() else air_strafe_accel
 
-	velocity.x = move_toward(velocity.x, target_x_speed, x_accel * delta)
-	velocity.x -= velocity.x * floor_friction * delta
+		velocity.x = move_toward(velocity.x, target_x_speed, x_accel * delta)
+		velocity.x -= velocity.x * floor_friction * delta
 
 	var was_on_floor := is_on_floor()
+	var prev_velocity := velocity
 	move_and_slide()
-	if not was_on_floor and is_on_floor():
+	if not _is_in_ground and not was_on_floor and is_on_floor():
 		animation_player.stop()
 		animation_player.play("land")
+
+	if not _is_in_ground:
+		var c := get_last_slide_collision()
+		if c and prev_velocity.length() >= min_speed_to_enter_ground:
+			dig_effect_spawner.global_position = c.get_position()
+			dig_effect_spawner.global_rotation = c.get_normal().angle()
+			velocity = prev_velocity.normalized() * dig_speed
+			enter_ground()
 
 func stop_holding_jump() -> void:
 	_is_holding_jump = false
