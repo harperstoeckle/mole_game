e46683a53d0cb9921684701046ef04927c75547a
Harper Stoeckle <harper.stoeckle@gmail.com>
2026-06-13 20:04:36 -0600

    Add a proper load transition animation

--- scripts/main.gd
+++ scripts/main.gd
@@ -10,6 +10,7 @@ extends Node2D
 @onready var _resume_button: Label = $UI/Menu/HBoxContainer/ResumeButton
 @onready var _quit_button: Label = $UI/Menu/HBoxContainer/QuitButton
 @onready var _main_menu_button: Label = $UI/Menu/HBoxContainer/MainMenuButton
+@onready var _ui_animation_player: AnimationPlayer = $UIAnimationPlayer
 
 
 var _level: Node = null
@@ -21,17 +22,17 @@ func _ready() -> void:
 
 func _unhandled_input(e: InputEvent) -> void:
 	# Don't allow toggling the menu if we're not in a level (in the main menu).
-	if e.is_action_pressed("pause") and _level:
+	if e.is_action_pressed("pause") and _level and not _is_in_transition():
 		_menu.visible = not _menu.visible
 		_update_game_pause()
 
 # Load a level from a `PackedScene` and play a transition animation.
 func load_level_from_packed(level_packed_scene: PackedScene) -> void:
-	if level_packed_scene: _set_level(level_packed_scene.instantiate())
-	else: _set_level(null)
+	var level: Node = level_packed_scene.instantiate() if level_packed_scene else null
 
 	_menu.visible = not _level
-	_update_menu_buttons()
+
+	_do_in_transition(func() -> void: _set_level(level))
 
 # Set the level immediately.
 func _set_level(level: Node) -> void:
@@ -39,9 +40,11 @@ func _set_level(level: Node) -> void:
 		node.queue_free()
 	_level = level
 	if _level: _level_container.add_child(_level)
+	_update_menu_buttons()
+	if not _level: _menu.show()
 
 func _update_game_pause() -> void:
-	get_tree().paused = _menu.visible
+	get_tree().paused = _menu.visible or _is_in_transition()
 
 func _update_menu_buttons() -> void:
 	var is_in_level := _level != null
@@ -49,6 +52,23 @@ func _update_menu_buttons() -> void:
 	_resume_button.visible = is_in_level
 	_quit_button.visible = not is_in_level
 	_main_menu_button.visible = is_in_level
+
+func _is_in_transition() -> bool:
+	return _ui_animation_player.is_playing()
+
+# Play a transition animation, and call `proc` in the middle. Will do nothing if a transition is currently playing.
+func _do_in_transition(f: Callable) -> void:
+	if _is_in_transition(): return
+
+	_ui_animation_player.play("transition_enter")
+	await _ui_animation_player.animation_finished
+	_update_game_pause()
+
+	f.call()
+	_ui_animation_player.play("transition_leave")
+	await _ui_animation_player.animation_finished
+	_update_game_pause()
+
 
 func _on_play_button_pressed() -> void:
 	load_level_from_packed(first_level)
